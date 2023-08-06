76f9a875fa33800696ed34e1387c9fc73daa09c5
k.pakalski <k.pakalski@advox.pl>
2023-08-06 17:22:14 +0200

    feature/FF-6

--- api/views/fun_fact_view_set.py
+++ api/views/fun_fact_view_set.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.serializers.fun_fact_serializer import FunFactSerializer
+from api.services.permissions.delete_permission import DeleteWithAPIKeyPermission
+
 from fun_facts.models import FunFact
 
 
@@ -10,6 +12,7 @@ class FunFactViewSet(ModelViewSet):
     serializer_class = FunFactSerializer
     queryset = FunFact.objects.all()
     http_method_names = ['get', 'post', 'delete']
+    permission_classes = [DeleteWithAPIKeyPermission]
 
     def retrieve(self, request, *args, **kwargs) -> Response:
         raise MethodNotAllowed('GET')
